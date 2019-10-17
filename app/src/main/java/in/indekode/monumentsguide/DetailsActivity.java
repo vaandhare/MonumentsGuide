61523dc5930f60e7a6224cc202bcc1a15956cb01
Vaibhav Andhare <vaibhavandhare7@gmail.com>
2019-10-17 20:44:15 +0530

    Directions of map removed and static link is added.

--- app/src/main/java/in/indekode/monumentsguide/DetailsActivity.java
+++ app/src/main/java/in/indekode/monumentsguide/DetailsActivity.java
@@ -1,24 +1,20 @@
 package in.indekode.monumentsguide;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.location.Location;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.Locale;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -51,19 +47,9 @@ public class DetailsActivity extends AppCompatActivity {
         MMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                locationManager=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
-                if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
-                    OnGPS();
-                else
-                    getLocation();
-
-                Intent i = new Intent(DetailsActivity.this, MapsActivity.class);
-                i.putExtra("Lang", String.valueOf(lang));
-                i.putExtra("Lat", String.valueOf(lat));
-                i.putExtra("NameM", NameM);
-                i.putExtra("CurrLang", String.valueOf(currLang));
-                i.putExtra("CurrLat", String.valueOf(currLat));
-                startActivity(i);
+                String uri = String.format(Locale.ENGLISH, "http://maps.google.com/maps?daddr=%f,%f", lat,lang);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                startActivity(intent);
             }
         });
 
@@ -77,65 +63,10 @@ public class DetailsActivity extends AppCompatActivity {
     private void initialize() {
         MImg = findViewById(R.id.MonumentImage);
         MMap = findViewById(R.id.MAP);
-
         MName = findViewById(R.id.MonumentName);
         MAdd = findViewById(R.id.Address);
         MLoc = findViewById(R.id.MapLocation);
         MEntryF = findViewById(R.id.EntryFee);
         MDetails = findViewById(R.id.Description);
-    }
-
-
-    private void getLocation() {
-        //Check Permissions again
-        if (ActivityCompat.checkSelfPermission(DetailsActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(DetailsActivity.this,
-                Manifest.permission.ACCESS_COARSE_LOCATION) !=PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this,new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-        }
-        else {
-            Location LocationGps= locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            Location LocationNetwork=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            Location LocationPassive=locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
-
-            if (LocationGps !=null) {
-                currLat=LocationGps.getLatitude();
-                currLang=LocationGps.getLongitude();
-            }
-            else if (LocationNetwork !=null) {
-                currLat=LocationNetwork.getLatitude();
-                currLang=LocationNetwork.getLongitude();
-            }
-            else if (LocationPassive !=null) {
-                currLat=LocationPassive.getLatitude();
-                currLang=LocationPassive.getLongitude();
-
-            }
-            else {
-                Toast.makeText(this, "Can't Get Your Location", Toast.LENGTH_SHORT).show();
-            }
-        }
-
-    }
-
-    private void OnGPS() {
-
-        final AlertDialog.Builder builder= new AlertDialog.Builder(this);
-
-        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("YES", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-            }
-        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                dialog.cancel();
-            }
-        });
-        final AlertDialog alertDialog=builder.create();
-        alertDialog.show();
     }
 }
